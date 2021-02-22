9df3fb24a20b28ac834094ec5d8c9e174e384ccd
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-02-22 20:03:14 -0300

    feat: add initial setup

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => response.json({message: "Hello Word"}))
+
+app.post('/', (request, response) => response.json({message: "Usuário criado com sucesso"}))
+
+export {app}
